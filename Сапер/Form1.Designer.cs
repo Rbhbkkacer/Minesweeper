6301ccae3ed1469f360ed628383ab8370efbdf03
Кирилл <bgfnjd94@outlook.com>
2015-11-27 16:15:45 +0400

    The first working version

---
+++ Сапер/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace Сапер
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.новаяИграToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.видToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.легкоToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.нормальноToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сложноToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.своёToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.видToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.menuStrip1.Size = new System.Drawing.Size(403, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.новаяИграToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.toolStripMenuItem2});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(53, 20);
+            this.toolStripMenuItem1.Text = "Меню";
+            // 
+            // новаяИграToolStripMenuItem
+            // 
+            this.новаяИграToolStripMenuItem.Name = "новаяИграToolStripMenuItem";
+            this.новаяИграToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.новаяИграToolStripMenuItem.Text = "Новая игра";
+            this.новаяИграToolStripMenuItem.Click += new System.EventHandler(this.новаяИграToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(133, 6);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(136, 22);
+            this.toolStripMenuItem2.Text = "Выход";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // видToolStripMenuItem
+            // 
+            this.видToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.легкоToolStripMenuItem,
+            this.нормальноToolStripMenuItem,
+            this.сложноToolStripMenuItem,
+            this.своёToolStripMenuItem});
+            this.видToolStripMenuItem.Name = "видToolStripMenuItem";
+            this.видToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.видToolStripMenuItem.Text = "Вид";
+            // 
+            // легкоToolStripMenuItem
+            // 
+            this.легкоToolStripMenuItem.Name = "легкоToolStripMenuItem";
+            this.легкоToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.легкоToolStripMenuItem.Text = "Легко";
+            this.легкоToolStripMenuItem.Click += new System.EventHandler(this.легкоToolStripMenuItem_Click);
+            // 
+            // нормальноToolStripMenuItem
+            // 
+            this.нормальноToolStripMenuItem.Name = "нормальноToolStripMenuItem";
+            this.нормальноToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.нормальноToolStripMenuItem.Text = "Нормально";
+            this.нормальноToolStripMenuItem.Click += new System.EventHandler(this.нормальноToolStripMenuItem_Click);
+            // 
+            // сложноToolStripMenuItem
+            // 
+            this.сложноToolStripMenuItem.Name = "сложноToolStripMenuItem";
+            this.сложноToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.сложноToolStripMenuItem.Text = "Сложно";
+            this.сложноToolStripMenuItem.Click += new System.EventHandler(this.сложноToolStripMenuItem_Click);
+            // 
+            // своёToolStripMenuItem
+            // 
+            this.своёToolStripMenuItem.Name = "своёToolStripMenuItem";
+            this.своёToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.своёToolStripMenuItem.Text = "Своё...";
+            this.своёToolStripMenuItem.Click += new System.EventHandler(this.своёToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(0, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.label2.Location = new System.Drawing.Point(351, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 331);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Сапер";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem новаяИграToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem видToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem легкоToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem нормальноToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сложноToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem своёToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Сапер/Form1.cs
@@ -0,0 +1,374 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Сапер
+{
+    public partial class Form1 : Form
+    {
+        Button[][] board;
+        byte x, y, b;
+        byte n = 30;
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            легкоToolStripMenuItem_Click(new object(), new EventArgs());
+        }
+
+        private void новаяИграToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            newgame();
+        }
+
+        private void newgame()
+        {
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    board[i][j].Enabled = true;
+                    board[i][j].BackColor = Color.DimGray;
+                    board[i][j].Tag = 0;
+                    board[i][j].RightToLeft = 0;
+                    board[i][j].Text = "";
+                }
+            }
+            ranndom();
+            for (byte q = 0; q < x; q++)
+            {
+                for (byte w = 0; w < y; w++)
+                {
+                    byte[] b = new byte[2];
+                    b[0] = q;
+                    b[1] = w;
+                    Thread t = new Thread(check);
+                    t.Start(b);
+                    //check(q, w);
+                }
+            }
+            timer1.Enabled = true;
+            label2.Text = "0";
+            label1.Text = Convert.ToString(b);
+        }
+
+        private void clear()
+        {
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    board[i][j].Dispose();
+                }
+            }
+        }
+
+
+
+        private void ranndom()
+        {
+            Random r = new Random();
+            for (int i = 0; i < b; i++)
+            {
+                int q;
+                int w;
+                while(board[q=r.Next(x)][w = r.Next(y)].RightToLeft == RightToLeft.Yes){}
+                board[q][w].RightToLeft = RightToLeft.Yes;
+                //board[q][w].BackColor = Color.Red;
+            }
+        }
+
+        private void легкоToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            clear();
+            x = 9;
+            y = 9;
+            b = 10;
+            setboard(y, x);
+            newgame();
+        }
+
+        private void нормальноToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            clear();
+            x = 16;
+            y = 16;
+            b = 40;
+            setboard(y, x);
+            newgame();
+        }
+
+        private void сложноToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            clear();
+            x = 16;
+            y = 30;
+            b = 99;
+            setboard(y, x);
+            newgame();
+        }
+
+        private void своёToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void setboard(byte x,byte y)
+        {
+            board = new Button[y][];
+            for(int i=0;i< y; i++)
+            {
+                board[i] = new Button[x];
+                for(int j=0;j< x; j++)
+                {
+                    board[i][j] = new Button();
+                    board[i][j].Parent = this;
+                    board[i][j].Name = String.Format("{0} ", x * i + j);
+                    board[i][j].FlatStyle = FlatStyle.Flat;
+                    board[i][j].Text = "";
+                    board[i][j].Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+                    board[i][j].Left = j * n;
+                    board[i][j].Width = n;
+                    board[i][j].Top = label1.Bottom + i * n;
+                    board[i][j].Height = n;
+                    board[i][j].Tag = 0;
+                    board[i][j].RightToLeft = RightToLeft.No;
+                    board[i][j].MouseDown += Form1_MouseClick;
+                    board[i][j].Enabled = true;
+                    board[i][j].BackColor = Color.DimGray;
+                    board[i][j].TabStop = false;
+                }
+            }
+            this.Width = x * n+16;
+            this.Height = y * n+40+board[0][0].Top;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label2.Text = Convert.ToString(Convert.ToInt32(label2.Text) + 1);
+        }
+        
+        private void check(object b)
+        {
+            byte[] b1 = (byte[])b;
+            byte x = b1[0];
+            byte y = b1[1];
+            try
+            {
+                if (board[x - 1][y - 1].RightToLeft == RightToLeft.Yes)
+                {
+                    board[x][y].Tag = (int)board[x][y].Tag + 1;
+                }
+            }
+            catch (Exception)
+            {
+            }
+            try
+            {
+                if (board[x - 1][y].RightToLeft == RightToLeft.Yes)
+                {
+                    board[x][y].Tag = (int)board[x][y].Tag + 1;
+                }
+            }
+            catch (Exception)
+            {
+            }
+            try
+            {
+                if (board[x - 1][y + 1].RightToLeft == RightToLeft.Yes)
+                {
+                    board[x][y].Tag = (int)board[x][y].Tag + 1;
+                }
+            }
+            catch (Exception)
+            {
+            }
+            try
+            {
+                if (board[x][y - 1].RightToLeft == RightToLeft.Yes)
+                {
+                    board[x][y].Tag = (int)board[x][y].Tag + 1;
+                }
+            }
+            catch (Exception)
+            {
+            }
+            try
+            {
+                if (board[x][y + 1].RightToLeft == RightToLeft.Yes)
+                {
+                    board[x][y].Tag = (int)board[x][y].Tag + 1;
+                }
+            }
+            catch (Exception)
+            {
+            }
+            try
+            {
+                if (board[x + 1][y - 1].RightToLeft == RightToLeft.Yes)
+                {
+                    board[x][y].Tag = (int)board[x][y].Tag + 1;
+                }
+            }
+            catch (Exception)
+            {
+            }
+            try
+            {
+                if (board[x + 1][y].RightToLeft == RightToLeft.Yes)
+                {
+                    board[x][y].Tag = (int)board[x][y].Tag + 1;
+                }
+            }
+            catch (Exception)
+            {
+            }
+            try
+            {
+                if (board[x + 1][y + 1].RightToLeft == RightToLeft.Yes)
+                {
+                    board[x][y].Tag = (int)board[x][y].Tag + 1;
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void click(object sender)
+        {
+            MouseEventArgs e = new MouseEventArgs(MouseButtons.Left, 1, 0, 0, 0);
+            Form1_MouseClick(sender, e);
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (((Button)sender).Enabled)
+            {
+                int w = Convert.ToInt32(((Button)sender).Name) % y;
+                int q = Convert.ToInt32(((Button)sender).Name) / y;
+                switch (e.Button)
+                {
+                    case (MouseButtons.Left):
+                        {
+                            if (((Button)sender).BackColor != Color.Green)
+                            {
+                                if (((Button)sender).RightToLeft == RightToLeft.No)
+                                {
+                                    ((Button)sender).Enabled = false;
+                                    ((Button)sender).BackColor = Color.White;
+                                    if ((int)((Button)sender).Tag == 0)
+                                    {
+                                        try
+                                        {
+                                            Form1_MouseClick(board[q - 1][w - 1], e);
+                                        }
+                                        catch (Exception)
+                                        {
+                                        }
+                                        try
+                                        {
+                                            Form1_MouseClick(board[q - 1][w], e);
+                                        }
+                                        catch (Exception)
+                                        {
+                                        }
+                                        try
+                                        {
+                                            Form1_MouseClick(board[q - 1][w + 1], e);
+                                        }
+                                        catch (Exception)
+                                        {
+                                        }
+                                        try
+                                        {
+                                            Form1_MouseClick(board[q][w - 1], e);
+                                        }
+                                        catch (Exception)
+                                        {
+                                        }
+                                        try
+                                        {
+                                            Form1_MouseClick(board[q][w + 1], e);
+                                        }
+                                        catch (Exception)
+                                        {
+                                        }
+                                        try
+                                        {
+                                            Form1_MouseClick(board[q + 1][w - 1], e);
+                                        }
+                                        catch (Exception)
+                                        {
+                                        }
+                                        try
+                                        {
+                                            Form1_MouseClick(board[q + 1][w], e);
+                                        }
+                                        catch (Exception)
+                                        {
+                                        }
+                                        try
+                                        {
+                                            Form1_MouseClick(board[q + 1][w + 1], e);
+                                        }
+                                        catch (Exception)
+                                        {
+                                        }
+
+                                    }
+                                    else
+                                    {
+                                        board[q][w].Text = board[q][w].Tag.ToString();
+                                    }
+
+                                }
+                                else
+                                {
+                                    timer1.Enabled = false;
+                                    board[q][w].BackColor = Color.Red;
+                                    for (int i = 0; i < x; i++)
+                                    {
+                                        for (int j = 0; j < y; j++)
+                                        {
+                                            board[i][j].Enabled = false;
+                                        }
+                                    }
+                                }
+                                
+                            }break;
+                        }
+                    case (MouseButtons.Right):
+                        {
+
+                            if (board[q][w].BackColor == Color.Green)
+                            {
+                                board[q][w].BackColor = Color.DimGray;
+                                label1.Text = Convert.ToString(Convert.ToInt32(label1.Text) + 1);
+                            }
+                            else
+                            {
+                                if (Convert.ToInt32(label1.Text) > 0)
+                                {
+                                    board[q][w].BackColor = Color.Green;
+                                    label1.Text = Convert.ToString(Convert.ToInt32(label1.Text) - 1);
+                                }
+                            }
+                            break;
+                        }
+                }
+            }
+        }
+    }
+}
